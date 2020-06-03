83ab186bd1075075943632bdb67f8c2fa4c776a2
Mariamol James <mariamol.james@gmail.com>
2020-06-03 16:26:25 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,16 +1,15 @@
 def mario
   puts "It's-a me, Mario!"
 end
-
 def toadstool
   puts 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
 
 def link
-  'It's Dangerous To Go Alone! Take This.'
+  puts "It's Dangerous To Go Alone! Take This."
 end
 
-def all phrases
+def all_phrases
   puts mario
   puts toadstool
   puts link
